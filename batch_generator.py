95ccb423ecd87be4d9750a89b1367f68dc1c849d
Anna Wang <annawangx@gmail.com>
2017-03-07 14:33:36 -0800

    fixed batch

--- batch_generator.py
+++ batch_generator.py
@@ -1,16 +1,16 @@
 from read_in_datafile import file_generator
 from embedding_wrapper import EmbeddingWrapper
 
-def batch_generator(embedding_wrapper, bill_data_path, indices_data_path, batch_size, MAX_BILL_LENGTH):
+def batch_generator(embedding_wrapper, bill_data_path, indices_data_path, sequences_data_path, batch_size, MAX_BILL_LENGTH):
 
-    f_generator = file_generator(batch_size, bill_data_path, indices_data_path)
+    f_generator = file_generator(batch_size, bill_data_path, indices_data_path, sequences_data_path)
 
     #pad the bills and summaries
     print "now padding and encoding batches"
     padded_bills = []
     padded_indices = []
     padded_masks = []
-    for bill_batch, indices_batch in f_generator:
+    for bill_batch, indices_batch, sequences in f_generator:
         # print "batch"
         # print bill_batch
         #print indices_batch
@@ -28,25 +28,20 @@ def batch_generator(embedding_wrapper, bill_data_path, indices_data_path, batch_
                 mask.append(False)
 
             padded_masks.append(mask)
-            padded_bill_of_one_hots = [[0 for y in range(0, embedding_wrapper.num_tokens)] for x in range(0,MAX_BILL_LENGTH)]
-            for idx, id_word_representation in enumerate(padded_bill):
-                padded_bill_of_one_hots[idx][id_word_representation] = 1
-            padded_bills.append(padded_bill_of_one_hots)
+            padded_bills.append(padded_bill)
 
             index_one_hot = [0] * MAX_BILL_LENGTH
-            if start_index > MAX_BILL_LENGTH:
+            if start_index >= MAX_BILL_LENGTH:
                 index_one_hot[0] = 1
             else:
                 index_one_hot[start_index] = 1
-            if end_index > MAX_BILL_LENGTH:
+            if end_index >= MAX_BILL_LENGTH:
                 index_one_hot[MAX_BILL_LENGTH - 1] = 1
             else:
                 index_one_hot[end_index] = 1
 
             padded_indices.append(index_one_hot)
-
-        print padded_indices
-        yield padded_bills, padded_indices, padded_masks
+        yield padded_bills, padded_indices, padded_masks, sequences
         padded_bills = []
         padded_indices = []
         padded_masks = []
