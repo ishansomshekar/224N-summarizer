d1137d34add6e872f24a43e188f6c6cefe47b8f2
Ishan <ishans@stanford.edu>
2017-03-14 22:56:18 -0700

    rouge testing script

--- rouge_script.py
+++ rouge_script.py
@@ -4,11 +4,11 @@ import csv
 import nltk
 import os.path
 
+model_name = "pointer_network_CE"
 
-
-with open('ROUGE_SCORES_MODEL.csv', 'wb') as csvfile:
+with open(file_name + 'csv', 'wb') as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(["rouge_l", "rouge_1", "rouge_2", "rouge_3", "bleu_score"])
+    writer.writerow(["rouge_l", "rouge_1", "rouge_3", "bleu_score"])
     count = 0
 
     with open('last_results.txt', 'r') as f:
@@ -21,10 +21,10 @@ with open('ROUGE_SCORES_MODEL.csv', 'wb') as csvfile:
             if gen != '' or gold != '':
                 rouge_l_score = rs.rouge_l(gen, [gold], 0.5)
                 rouge_1_score = rs.rouge_n(gen, [gold], 1, 0.5)
-                rouge_2_score = rs.rouge_n(gen, [gold], 2, 0.5)
+                # rouge_2_score = rs.rouge_n(gen, [gold], 2, 0.5)
                 rouge_3_score = rs.rouge_n(gen, [gold], 3, 0.5)
                 bleu_score = nltk.translate.bleu_score.corpus_bleu([gen], [gold]) 
-            writer.writerow([rouge_l_score, rouge_1_score, rouge_2_score, rouge_3_score, bleu_score])
+            writer.writerow([rouge_l_score, rouge_1_score, rouge_3_score, bleu_score])
             count += 1
             if count % 1000 == 0:
                 print count
