4eec26e9a7cd3ed529fea47a84e96cf4ed8c905f
Ishan <ishans@stanford.edu>
2017-03-14 22:39:34 -0700

    rouge testing script

---
+++ rouge_script.py
@@ -0,0 +1,33 @@
+import rougescore as rs
+import glob
+import csv
+import nltk
+import os.path
+
+
+
+with open('ROUGE_SCORES_MODEL.csv', 'wb') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["rouge_l", "rouge_1", "rouge_2", "rouge_3", "bleu_score"])
+    count = 0
+
+    with open('last_results.txt', 'r') as f:
+        while True:
+            gen = f.readline()
+            gold = f.readline()
+            if not gold: 
+                break
+
+            if gen != '' or gold != '':
+                rouge_l_score = rs.rouge_l(gen, [gold], 0.5)
+                rouge_1_score = rs.rouge_n(gen, [gold], 1, 0.5)
+                rouge_2_score = rs.rouge_n(gen, [gold], 2, 0.5)
+                rouge_3_score = rs.rouge_n(gen, [gold], 3, 0.5)
+                bleu_score = nltk.translate.bleu_score.corpus_bleu([gen], [gold]) 
+            writer.writerow([rouge_l_score, rouge_1_score, rouge_2_score, rouge_3_score, bleu_score])
+            count += 1
+            if count % 1000 == 0:
+                print count
+
+
+
