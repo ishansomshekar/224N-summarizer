5354819afc1eee17f726d1efff6a0308ef16f5b9
Anna Wang <annawangx@gmail.com>
2017-03-16 14:46:18 -0700

    prep for run

--- train_pointer_network_lin_dep.py
+++ train_pointer_network_lin_dep.py
@@ -33,9 +33,9 @@ class SequencePredictor():
 
         self.glove_dim = 50
         self.num_epochs = 10
-        self.bill_length = 251
+        self.bill_length = 301
         self.keywords_length = 5
-        self.lr = 0.0005
+        self.lr = 0.0001
         self.inputs_placeholder = None
         self.summary_input = None
         self.summary_op = None
@@ -56,25 +56,25 @@ class SequencePredictor():
         self.vocab_size = embedding_wrapper.num_tokens
         self.embedding_init = None
 
-        self.train_data_file = "bills_train_bills_5_250.txt" #"train_bills_3_context.txt"
-        self.train_summary_data_file = "summaries_train_bills_5_250.txt"
-        self.train_indices_data_file = "indices_train_bills_5_250.txt"
-        self.train_sequence_data_file = "sequences_train_bills_5_250.txt"
+        self.train_data_file = "bills_train_bills_6_300.txt" #"train_bills_3_context.txt"
+        self.train_summary_data_file = "summaries_train_bills_6_300.txt"
+        self.train_indices_data_file = "indices_train_bills_6_300.txt"
+        self.train_sequence_data_file = "sequences_train_bills_6_300.txt"
         self.train_keyword_data_file = "train_bills_5_keywords.txt"
         file_open = open(self.train_data_file, 'r')
         self.train_len = len(file_open.read().split("\n"))
         file_open.close()
 
-        self.dev_data_file =  "bills_dev_bills_5_250.txt"
-        self.dev_summary_data_file =  "summaries_dev_bills_5_250.txt"
-        self.dev_indices_data_file = "indices_dev_bills_5_250.txt"
-        self.dev_sequence_data_file = "sequences_dev_bills_5_250.txt"
+        self.dev_data_file =  "bills_dev_bills_6_300.txt"
+        self.dev_summary_data_file =  "summaries_dev_bills_6_300.txt"
+        self.dev_indices_data_file = "indices_dev_bills_6_300.txt"
+        self.dev_sequence_data_file = "sequences_dev_bills_6_300.txt"
         self.dev_keyword_data_file = "dev_bills_3_keywords.txt"
 
-        self.test_data_file =  "bills_test_bills_5_250.txt"
-        self.test_summary_data_file =  "summaries_test_bills_5_250.txt"
-        self.test_indices_data_file = "indices_test_bills_5_250.txt"
-        self.test_sequence_data_file = "sequences_test_bills_5_250.txt"
+        self.test_data_file =  "bills_test_bills_6_300.txt"
+        self.test_summary_data_file =  "summaries_test_bills_6_300.txt"
+        self.test_indices_data_file = "indices_test_bills_6_300.txt"
+        self.test_sequence_data_file = "sequences_test_bills_6_300.txt"
         self.test_keyword_data_file = "test_bills_3_keywords.txt"
 
 
